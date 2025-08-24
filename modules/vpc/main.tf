3011e3d8ff7c1e5aac3b06c6d56b8ce71f05a2e1
selvan-terraform <selvan.chidambaram1978@gmail.com>
2025-08-24 14:31:08 +0530

    Update

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -8,3 +8,12 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.public_subnet_cidr
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = "${var.vpc_name}-public"
+  }
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,9 +1,21 @@
 variable "vpc_cidr_block" {
   description = "The CIDR block for the VPC"
   type        = string
+  default     = "10.0.0.0/16"
 }
 
 variable "vpc_name" {
   description = "The name of the VPC"
   type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "The CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+variable "availability_zone" {
+  description = "The availability zone for the subnet"
+  type        = string
+  default     = "us-east-1a"
 }
